9605ad8b80824ec5e2b1fe7b36e17d609316111f
wizardxxdx <wizardxd@icloud.com>
2022-09-10 16:57:13 -0700

    CHE

--- crawler.py
+++ crawler.py
@@ -1,14 +1,29 @@
 #!/usr/bin/env python3
 import requests
 
-URL = "https://mmmm.google.com"
+URL = "google.com"
+
 
 def request(url):
     try:
-        response = requests.get(url=URL)
-        print(response)
-
+        return requests.get("http://" + url)
 
     except requests.exceptions.ConnectionError:
         pass
 
+# with open("subdomains-wodlist.txt", "r") as wordlist_file:
+#     words = [line.strip() for line in wordlist_file]
+#     for word in words:
+#         test_url = f"{word}.{URL}"
+#         response = request(test_url)
+#         if response:
+#             print(f"[+] Discovered subdomain --> {test_url}")
+
+
+with open("files-and-dirs-wordlist.txt", "r") as wordlist_file:
+    words = [line.strip() for line in wordlist_file]
+    for word in words:
+        test_url = f"{word}.{URL}"
+        response = request(test_url)
+        if response:
+            print(f"[+] Discovered subdomain --> {test_url}")
