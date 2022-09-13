2d17f35d3e3f081b73f26eacdfbc077744709f98
wizardxxdx <wizardxd@icloud.com>
2022-09-12 22:34:21 -0700

    CHE

--- crawler.py
+++ crawler.py
@@ -2,8 +2,6 @@
 import requests
 import argparse
 
-URL = "google.com"
-
 
 def request(url):
     try:
@@ -13,23 +11,36 @@ def request(url):
         pass
 
 
-def get_subdomain():
+def get_subdomain(domain):
     with open("subdomains-wodlist.txt", "r") as wordlist_file:
         words = [line.strip() for line in wordlist_file]
         for word in words:
-            print(f"{word}.{URL}")
-            test_url = f"{word}.{URL}"
+            test_url = f"{word}.{domain}"
             response = request(test_url)
             if response:
                 print(f"[+] Discovered subdomain --> {test_url}")
 
 
+# def get_dir(directory):
+#     with open("files-and-dirs-wordlist.txt", "r") as wordlist_file:
+#         words = [line.strip() for line in wordlist_file]
+#         for word in words:
+#             test_url = f"{URL}/{word}"
+#             response = request(test_url)
+#             if response:
+#                 print(f"[+] Discovered URL --> {test_url}")
+
+
 parser = argparse.ArgumentParser(description='To find subdomain')
-parser.add_argument('-d', '--domain', dest='url', help='[+] write domain google.com')
+parser.add_argument('-d', '--domain', help='[+] write domain google.com')
 
 args = parser.parse_args()
-print(args.domain)
 
+target_domain = args.domain
+
+print(target_domain)
+
+get_subdomain(domain=target_domain)
 
 # with open("files-and-dirs-wordlist.txt", "r") as wordlist_file:
 #     words = [line.strip() for line in wordlist_file]
