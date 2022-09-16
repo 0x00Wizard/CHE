876d71b6697f97764e85dafbb94bce5abeffe4ba
wizardxxdx <wizardxd@icloud.com>
2022-09-16 00:29:12 -0700

    CHE

--- vuln_scanner/scanner.py
+++ vuln_scanner/scanner.py
@@ -1,18 +1,19 @@
 #!/usr/bin/env python3
 import requests
 import re
+from bs4 import BeautifulSoup
 from urllib.parse import urljoin
-
-target_url = "https://zsecurity.org/"
 
 
 class Scanner:
-    def __int__(self, url):
+    def __init__(self, url, link_to_ignore):
+        self.session = requests.session()
         self.target_url = url
         self.target_links = []
+        self.link_to_ignore = link_to_ignore
 
     def extract_link(self, url):
-        response = requests.get(url)
+        response = self.session.get(url)
         return re.findall('(?:href=")(.*?)"', response.text)
 
     def crawl(self, url=None):
@@ -24,7 +25,43 @@ class Scanner:
             if "#" in link:
                 link = link.split("#")[0]
 
-            if self.target_url in link and link not in self.target_links:
+            if self.target_url in link and link not in self.target_links and link not in self.link_to_ignore:
                 self.target_links.append(link)
                 print(link)
                 self.crawl(link)
+
+    def extract_forms(self, url):
+        response = self.session.get(url)
+        html_response = BeautifulSoup(response.text, "html.parser")
+        return html_response.find_all("form")
+
+    def submit_form(self, form, value, url):
+        action = form.get("action")
+        post_url = urljoin(url, action)
+        print(post_url)
+        method = form.get("method")
+        inputs_list = form.find_all("input")
+        post_data = {}
+        for inputs in inputs_list:
+            input_name = inputs.get("name")
+            input_type = inputs.get("type")
+            input_value = inputs.get("value")
+            if input_type == "text":
+                input_value = value
+
+            post_data[input_name] = input_value
+        if method == "post":
+            return requests.post(url=post_url, data=post_data)
+        return self.session.get(post_url, params=post_data)
+
+    def run_scanner(self):
+        for link in self.target_links:
+            forms = self.extract_forms(link)
+            for form in forms:
+                print(f"[+] Testing form in {form}")
+
+    def test_xss_in_form(self, form, url):
+        xss_test_script = "<script> alert('test') </script>"
+        response = self.submit_form(form, xss_test_script, url)
+        if xss_test_script in response.text:
+            return True
--- vuln_scanner/vuln_scanner.py
+++ vuln_scanner/vuln_scanner.py
@@ -2,8 +2,14 @@
 
 import scanner
 
-target_url = "https://zsecurity.org/"
 
-vuln_scanner = scanner.Scanner(target_url)
+target_url = "http://testphp.vulnweb.com/"
+data_dict = {"username": "admin", "password": "password", "Login": "submit"}
+links_to_ignore = ["logout.php"]
+vuln_scanner = scanner.Scanner(target_url, links_to_ignore)
+vuln_scanner.session.post(target_url, data=data_dict)
 
-vuln_scanner.crawl()
+# vuln_scanner.crawl()
+
+forms = vuln_scanner.extract_forms("http://localhost")
+response = vuln_scanner.submit_form(forms[0], "test")
